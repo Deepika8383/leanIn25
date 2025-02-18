bd7258c642aa023df23d123a6bd6fd5cd452e306
deepika8383 <jaindeepika8383@gmail.com>
2025-02-18 11:31:36 +0530

    bjsjdh

---
+++ controller/welcome.js
@@ -0,0 +1,7 @@
+const welcome= (req, res)=>{
+    try{
+        return res.json({message: "Welcome to Lean In @5 API!"});
+    }catch(error){
+        return res.status(500).json({error: error.message});
+    }
+}
