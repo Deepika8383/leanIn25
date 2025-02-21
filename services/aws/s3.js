f911e39809495b33f579ec1cdd2036dc340c260f
deepika8383 <jaindeepika8383@gmail.com>
2025-02-21 19:27:38 +0530

    eu

--- services/aws/s3.js
+++ services/aws/s3.js
@@ -3,7 +3,7 @@ require("dotenv").config(); // Load environment variables
 const { S3Client } = require("@aws-sdk/client-s3");
 
 const s3Client = new S3Client({
-    region: process.env.AWS_REGION,
+    region: "eu-north-1",
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
